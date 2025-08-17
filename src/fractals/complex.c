220efff8a0bed40f0a74032b70671ae9cb78b8d6
PRANIT4166 <121449375+PRANIT4166@users.noreply.github.com>
2025-08-18 04:14:49 +0530

    complex number calculations

--- src/fractals/complex.c
+++ src/fractals/complex.c
@@ -1,16 +1,29 @@
 #include "complex.h"
 
-struct complex_number
-{
-    int r;
-    int im;
-}typedef complex_num;
-
-
 complex_num complex_init_num(complex_num z, int r, int im)
 {
     z.r = r;
     z.im = im;
+}
+
+complex_num complex_addition(complex_num z1, complex_num z2)
+{
+    complex_num z;
+    double new_r = z1.r + z2.r;
+    double new_im = z1.im + z2.im;
+    
+    complex_init_num(z,new_r,new_im);
+    return z;
+}
+
+complex_num complex_squared(complex_num z1)
+{
+    complex_num z;
+    double new_r = z1.r*z1.r - z1.im*z1.im;
+    double new_im = (2*z1.r*z1.im) ;
+    
+    complex_init_num(z,new_r,new_im);
+    return z;
 }
 
 
