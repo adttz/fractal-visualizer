38d38ac6269ca02048d51672044e3cdc47c19307
PRANIT4166 <121449375+PRANIT4166@users.noreply.github.com>
2025-08-22 01:58:14 +0530

    ready header for complex calculations

--- src/fractals/MPFR_Complex_Math.h
+++ src/fractals/MPFR_Complex_Math.h
@@ -1,40 +1,62 @@
 #pragma once
 #include <mpfr.h>
-// ----Properties---------------
+
+//---------- return status ------------
+typedef enum
+{
+    SUCCESS = 0,  // operation completed successfully
+    NULL_POINTER, // null pointer passed
+    ERR_INIT,     // variable not initialized
+    ERR_NO_MEM,   // memory allocation failure
+    ERR_MATH,     // invalid math domain
+    ERR_UNKNOWN   // catch-all
+} status;
+
+// ----------- Properties ---------------
 
 typedef struct
 {
     mpfr_prec_t precision;
     mpfr_rnd_t round_mode;
-}var_prop;
+} var_prop;
 
-// ------------temp vars--------------
+// ------------ temp vars --------------
 
 typedef struct
 {
-    mpfr_t temp1, temp2, temp3, temp4; 
-}temp_var;
+    mpfr_t temp1, temp2, temp3, temp4;
+} temp_var;
 
-// --------complex var-----------
+// -------- complex var -----------
 
 typedef struct
 {
     mpfr_t r, im;
-}var_c;
+} var_c;
 
-// ---------WS initialising-----
+// --------- WS initialising -----
 
-void set_var_prop(var_prop* prop, mpfr_prec_t prec, mpfr_rnd_t rnd); 
-void set_temp_var(temp_var* temps, var_prop* prop);
-void clear_temp_var(temp_var* temps);
+status set_var_prop(var_prop *prop, const mpfr_prec_t prec, const mpfr_rnd_t rnd);
+status set_temp_var(temp_var *temps, const var_prop *prop);
+status clear_temp_var(temp_var *temps);
 
-// ----------- complex number initiliasing-------------
+// ----------- complex number initiliasing -------------
 
-void init_var_c(var_c* z, const var_prop* prop);
-void clear_var_c(var_c* z);
-void set_var_c(var_c* z_dst, var_c* z_src, var_prop* prop);
-void set_var_c_double(var_c* z_dst, double real, double img, var_prop* prop);
-void set_var_c_zero(var_c* z);
+status init_var_c(var_c *z, const var_prop *prop);
+status clear_var_c(var_c *z);
+status set_var_c(var_c *z_dst, const var_c *z_src, const var_prop *prop);
+status set_var_c_double(var_c *z_dst, const double real, const double img, const var_prop *prop);
+status set_var_c_zero(var_c *z);
 
-//-----------
+//----------- math ---------------------------
+status add_c(var_c *z_dst, const var_c *z1, const var_c *z2, const var_prop *var_prop);
+status sub_c(var_c *z_dst, const var_c *z1, const var_c *z2, const var_prop *var_prop);
+status mult_c(var_c *z_dst, const var_c *z1, const var_c *z2, const var_prop *var_prop, temp_var *temps);
+status sqr_c(var_c *z_dst, const var_c *z_src, const var_prop *var_prop, temp_var *temps);
+status conj_c(var_c *z_dst, const var_c *z_src, const var_prop *var_prop);
+status abs_c(mpfr_t dst, const var_c *z_src, const var_prop *var_prop, temp_var *temps);
 
+// ---------- debugs ------------------------
+status print_c(const var_c *z, const var_prop *prop);
+status print_c_prec(const var_c *z, const var_prop *prop, const int digits);
+status mpfrx_c_to_str(char *buf, size_t n, const var_c *z, const var_prop *prop, const int digits);
